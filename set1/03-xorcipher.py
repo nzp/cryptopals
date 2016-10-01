39c317af7f30a871e826092b2a828720d40bbd5e
Nikola Pavlović <nzp@riseup.net>
2016-10-01 17:10:15 +0200

    Update 03 for static get_most_probable.

--- set1/03-xorcipher.py
+++ set1/03-xorcipher.py
@@ -4,6 +4,6 @@ import xorcipher
 ciphertext = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
 
 txt = xorcipher.XorCiphertext(ciphertext)
-result = txt.get_most_probable()
+result = txt.get_most_probable(plaintexts=txt.get_plaintexts())
 print(result.plaintext, result.key)
 
